3f2f3d914fef6d3241d15c46d1573f94d6d1cb2e
MorphTail <chitniskshitij@gmail.com>
2021-05-12 23:39:58 +0530

    add templates

---
+++ pages/things-i-do/music.js
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  useColorMode,
+  Flex,
+  Stack,
+  Heading,
+  Divider,
+  Text,
+  Spacer,
+  HStack,
+} from "@chakra-ui/react";
+import Container from "../components/Container";
+
+const Gear = () => {
+  const { colorMode } = useColorMode();
+  const colorPrimary = {
+    light: "linear(to-br, #3399ff,#ff3399)",
+    dark: "linear(to-br, #33ccff,#ff55cc)",
+  };
+  const colorSecondary = {
+    light: "gray.700",
+    dark: "gray.400",
+  };
+
+  return (
+    <>
+      <Container>
+        <Stack
+          as="main"
+          spacing={8}
+          justifyContent="center"
+          alignItems="flex-start"
+          m="0 auto 4rem auto"
+          maxWidth="100%"
+        >
+          <Flex
+            flexDirection="column"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            maxWidth="100%"
+            px={4}
+          >
+           
+          </Flex>
+        </Stack>
+      </Container>
+    </>
+  );
+};
+
+export default Gear;
